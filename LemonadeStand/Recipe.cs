7131e41695a2a0559f19937cf1379a47020d8e2c
DavidRoszina <david.a.roszina@gmail.com>
2020-04-21 13:47:44 -0500

    completed Recipe

--- LemonadeStand/Recipe.cs
+++ LemonadeStand/Recipe.cs
@@ -6,5 +6,14 @@ namespace LemonadeStand
 {
     class Recipe
     {
+        public int lemonsToAdd;
+        public int iceCubesToAdd;
+        public int sugarCubesToAdd;
+        public Recipe()
+        {
+            lemonsToAdd = 6;
+            iceCubesToAdd = 8;
+            sugarCubesToAdd = 30;
+        }
     }
 }
